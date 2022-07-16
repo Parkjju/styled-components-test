0c1835ae6fbc2efd3fb37d044da68d62e8750f8a
kye1115z <kye1115z@naver.com>
2022-07-17 01:58:48 +0900

    kimye2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Register from './Register';
+import Login from './Login';
+import Registerrr from './Registerrr';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <Registerrr />
+        <Login />
         <Register />
     </React.StrictMode>
 );
